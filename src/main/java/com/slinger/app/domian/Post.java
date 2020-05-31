148069c18afefe8c8ab22007c3933f9ac3e0088f
brent90 <brentjbaumgart@gmail.com>
2020-05-31 12:57:25 -0500

    Created User, Post and Comment objects. Configured application.properties for H2 and logging. closes #1

---
+++ src/main/java/com/slinger/app/domian/Post.java
@@ -0,0 +1,23 @@
+package com.slinger.app.domian;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    @Lob
+    private String body;
+}
